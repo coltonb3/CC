534b7f1c4fa9c2248ad42d87a39fa41a84ef25e8
coltonb3 <clyb333@gmail.com>
2023-01-26 17:32:37 -0600

    package install

--- cham/src/App.js
+++ cham/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
+import NavBar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1> Test </h1>
+      <h1> Champagne Wishes & Charcuterie Dreams </h1>
+    <NavBar />    
+    <Header/>
+
     </div>
   );
 }
